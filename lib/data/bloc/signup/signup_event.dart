607a7a492bf1657d6335b0d42e1a1b9adb9a7d54
seonho99 <pandapad10@gmail.com>
2025-01-11 15:40:33 +0900

    feat: firebase Authentication 수정중

--- lib/data/bloc/signup/signup_event.dart
+++
@@ -1,9 +0,0 @@
-sealed class SignUpEvent {}
-
-final class SignUpWithEmail extends SignUpEvent {
-  final String email;
-  final String password;
-  final String name;
-
-  SignUpWithEmail({required this.email, required this.password, required this.name});
-}
