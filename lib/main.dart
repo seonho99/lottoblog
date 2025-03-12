27a6801f730ad0a744e1b07ed2ef8fbbe5c8ad5b
seonho99 <pandapad10@gmail.com>
2025-03-12 18:18:09 +0900

    fix: flutter upgrade 화면 수정 완료

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
+import 'package:flutter/rendering.dart'4;
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:lottoblog/data/bloc/like_post/like_post_bloc.dart';
 import 'package:lottoblog/data/bloc/login/login_bloc.dart';
